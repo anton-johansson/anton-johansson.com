0750ecc3220ba3dc65035b710956049ecbd227cb
Anton Johansson <antoon.johansson@gmail.com>
2019-02-09 18:13:11 +0100

    Fix warning and indentation

--- src/components/utils/render-social-links.js
+++ src/components/utils/render-social-links.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export default settings => {
-    return settings.socialLinks && settings.socialLinks.map(item => {
-        return (
-          <li>
-            <a href={item.url}>
-              <i className={item.className}/>
-            </a>
-          </li>
-        )
-      });
+  return settings.socialLinks && settings.socialLinks.map(item => {
+    return (
+      <li key={item.name}>
+        <a href={item.url}>
+          <i className={item.className}/>
+        </a>
+      </li>
+    )
+  });
 };
