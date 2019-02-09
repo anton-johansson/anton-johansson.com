6da7807c8880386d6cd5a64246a160f472871df1
Anton Johansson <antoon.johansson@gmail.com>
2019-02-09 17:57:51 +0100

    Minor code style tweaks

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { About, Contact, Footer, Header, Portfolio, Resume, Testimonials } from './components';
 import resumeData from './resumeData';
 import data from './data';
-import {translate} from './services';
-class App extends Component {
+import { translate } from './services';
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -18,5 +19,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
