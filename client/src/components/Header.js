05ff7a192ad1351afa0e4795a04c92591fcb87a4
Anton Johansson <antoon.johansson@gmail.com>
2019-03-24 17:38:49 +0100

    Add navigation for switching language

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import renderSocialLinks from './utils/render-social-links';
+import getLanguageCode from '../services/get-language-code';
+
+const getChangeLanguageData = () => {
+    const languageCode = getLanguageCode();
+    if (languageCode === 'en') {
+        return {
+            title: 'Svenska',
+            url: 'https://anton-johansson.se',
+            flag: 'sweden.png'
+        }
+    } else {
+        return {
+            title: 'English',
+            url: 'https://anton-johansson.com',
+            flag: 'united-kingdom.png'
+        }
+    }
+};
 
 export default class Header extends Component {
   render() {
     const {settings, translate} = this.props;
+    const changeLanguageData = getChangeLanguageData();
     return (
       <React.Fragment>
         <header id="home">
@@ -17,12 +36,13 @@ export default class Header extends Component {
               <li><a className="smoothscroll" href="#tools">{translate('navigation.section.tools')}</a></li>
               <li><a className="smoothscroll" href="#testimonials">{translate('navigation.section.testimonials')}</a></li>
               <li><a className="smoothscroll" href="#contact">{translate('navigation.section.contact')}</a></li>
+              <li><img id='language-flag' src={`/images/flags/${changeLanguageData.flag}`} alt={changeLanguageData.title}/><a href={changeLanguageData.url}>{changeLanguageData.title}</a></li>
             </ul>
           </nav>
           <div className="row banner">
             <div className="banner-text">
               <h1 className="responsive-headline">{translate('home.title')}</h1>
-              <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>{translate('home.description')}</h3>
+              <h3 style={{color:'#fff', fontFamily:'sans-serif'}}>{translate('home.description')}</h3>
               <hr/>
               <ul className="social">
                 {renderSocialLinks(settings)}
