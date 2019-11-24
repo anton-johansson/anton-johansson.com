e126b79ddb891f41c04fe8f7afb3e04f0c44c516
Anton Johansson <antoon.johansson@gmail.com>
2019-11-24 22:32:01 +0100

    Replace jQuery smooth scroll with React

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import renderSocialLinks from './utils/render-social-links';
 
 export default class Footer extends Component {
-  render() {
-    const {settings} = this.props;
-    return (
-      <footer>
-        <div className="row">
-          <div className="twelve columns">
-            <ul className="social-links">
-              {renderSocialLinks(settings)}
-            </ul>
-          </div>
-          <div id="go-top">
-            <a className="smoothscroll" title="Back to Top" href="#home">
-              <i className="icon-up-open"/>
-            </a>
-          </div>
-        </div>
-      </footer>
-    );
-  }
+    render() {
+        const {settings} = this.props;
+        return (
+            <footer>
+                <div className="row">
+                    <div className="twelve columns">
+                        <ul className="social-links">
+                            {renderSocialLinks(settings)}
+                        </ul>
+                    </div>
+                    <div id="go-top">
+                        <AnchorLink href="#home">
+                            <i className="icon-up-open"/>
+                        </AnchorLink>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
 }
--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactFitText from 'react-fittext';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 import renderSocialLinks from './utils/render-social-links';
 import getLanguageCode from '../services/get-language-code';
 
@@ -30,38 +31,40 @@ export default class Header extends Component {
         const changeLanguageData = getChangeLanguageData();
         return (
             <React.Fragment>
-            <header id="home">
-                <nav id="nav-wrap">
-                <a className="mobile-btn" href="#nav-wrap" title={translate('navigation.show')}>{translate('navigation.show')}</a>
-                <a className="mobile-btn" href="#nav-hide" title={translate('navigation.hide')}>{translate('navigation.hide')}</a>
-                <ul id="nav" className="nav">
-                    <li className="current"><a className="smoothscroll" href="#home">{translate('navigation.section.home')}</a></li>
-                    <li><a className="smoothscroll" href="#about">{translate('navigation.section.about')}</a></li>
-                    <li><a className="smoothscroll" href="#activity">{translate('navigation.section.activity')}</a></li>
-                    <li><a className="smoothscroll" href="#resume">{translate('navigation.section.resume')}</a></li>
-                    <li><a className="smoothscroll" href="#tools">{translate('navigation.section.tools')}</a></li>
-                    <li><a className="smoothscroll" href="#contact">{translate('navigation.section.contact')}</a></li>
-                    <li><img id='language-flag' src={`/images/flags/${changeLanguageData.flag}`} alt={changeLanguageData.title}/><a href={changeLanguageData.url}>{changeLanguageData.title}</a></li>
-                </ul>
-                </nav>
-                <div className="row banner">
-                <div className="banner-text">
-                    <ReactFitText minFontSize={40} maxFontSize={90}>
-                        <h1>
-                            {translate('home.title')}
-                        </h1>
-                    </ReactFitText>
-                    <h3 style={{color:'#fff', fontFamily:'sans-serif'}}>{translate('home.description')}</h3>
-                    <hr/>
-                    <ul className="social">
-                        {renderSocialLinks(settings)}
-                    </ul>
-                </div>
-                </div>
-                <p className="scrolldown">
-                <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-                </p>
-            </header>
+                <header id="home">
+                    <nav id="nav-wrap">
+                        <a className="mobile-btn" href="#nav-wrap" title={translate('navigation.show')}>{translate('navigation.show')}</a>
+                        <a className="mobile-btn" href="#nav-hide" title={translate('navigation.hide')}>{translate('navigation.hide')}</a>
+                        <ul id="nav" className="nav">
+                            <li className="current"><a className="smoothscroll" href="#home">{translate('navigation.section.home')}</a></li>
+                            <li><a className="smoothscroll" href="#about">{translate('navigation.section.about')}</a></li>
+                            <li><a className="smoothscroll" href="#activity">{translate('navigation.section.activity')}</a></li>
+                            <li><a className="smoothscroll" href="#resume">{translate('navigation.section.resume')}</a></li>
+                            <li><a className="smoothscroll" href="#tools">{translate('navigation.section.tools')}</a></li>
+                            <li><a className="smoothscroll" href="#contact">{translate('navigation.section.contact')}</a></li>
+                            <li><img id='language-flag' src={`/images/flags/${changeLanguageData.flag}`} alt={changeLanguageData.title}/><a href={changeLanguageData.url}>{changeLanguageData.title}</a></li>
+                        </ul>
+                    </nav>
+                    <div className="row banner">
+                        <div className="banner-text">
+                            <ReactFitText minFontSize={40} maxFontSize={90}>
+                                <h1>
+                                    {translate('home.title')}
+                                </h1>
+                            </ReactFitText>
+                            <h3 style={{color:'#fff', fontFamily:'sans-serif'}}>{translate('home.description')}</h3>
+                            <hr/>
+                            <ul className="social">
+                                {renderSocialLinks(settings)}
+                            </ul>
+                        </div>
+                    </div>
+                    <p className="scrolldown">
+                        <AnchorLink href="#about">
+                            <i className="icon-down-circle"></i>
+                        </AnchorLink>
+                    </p>
+                </header>
             </React.Fragment>
         );
     }
