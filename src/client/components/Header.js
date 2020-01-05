51fdb64fc8258ef2a8b027e354cbd0caa19741ba
Anton Johansson <antoon.johansson@gmail.com>
2020-01-05 14:33:37 +0100

    fix: Fix broken "change language" link

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -8,8 +8,9 @@ import { Waypoint } from 'react-waypoint';
 import Translate from './utils/Translate';
 
 const getChangeLanguageData = (languageCode, currentURL) => {
+    console.log(currentURL);
     const generateURL = newTopDomain => {
-        return currentURL.replace(/^(.*)(\.\w+)(\:\d+)?$/, `$1${newTopDomain}$3`);
+        return currentURL.replace(/^(.*)(\.\w+)(\:\d+)?(.*)$/, `$1${newTopDomain}$3`);
     }
     if (languageCode === 'en') {
         return {
